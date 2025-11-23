20500a821ea21a3285bc120c4e4d22291598337b
Your Name <you@example.com>
2025-11-23 16:38:55 +0530

    Changes upeated and added def call

--- vars/relBranchCut.groovy
+++ vars/relBranchCut.groovy
@@ -1,42 +1,45 @@
-pipeline {
-    agent {
-        node {
-            label 'local-agent'
-        }
-    }
+def call(Map config) {
 
-    parameters {
-        string(name: 'BRANCH_NAME', defaultValue: 'develop', description: 'Branch to checkout')
-        string(name: 'RELEASE_VERSION', defaultValue: 'rel_25.12-01.00', description: 'Release Version')
-
-    }
-
-    stages {
-        stage('Checkout') {
-            steps {
-                git branch: "${params.BRANCH_NAME}",
-                        url: 'git@github.com:charikajana/NimbusHotelBooker.git',
-                        credentialsId: 'ChariToken'
+    pipeline {
+        agent {
+            node {
+                label 'local-agent'
             }
         }
 
-        stage('Update POM Version') {
-            steps {
-                sh 'chmod +x scripts/update_pom.sh'
-                sh "./scripts/update_pom.sh ${params.RELEASE_VERSION}"
+        parameters {
+            string(name: 'BRANCH_NAME', defaultValue: 'develop', description: 'Branch to checkout')
+            string(name: 'RELEASE_VERSION', defaultValue: 'rel_25.12-01.00', description: 'Release Version')
+
+        }
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    git branch: "${params.BRANCH_NAME}",
+                            url: 'git@github.com:charikajana/NimbusHotelBooker.git',
+                            credentialsId: 'ChariToken'
+                }
+            }
+
+            stage('Update POM Version') {
+                steps {
+                    sh 'chmod +x scripts/update_pom.sh'
+                    sh "./scripts/update_pom.sh ${params.RELEASE_VERSION}"
+                }
+            }
+
+            stage('Maven Build') {
+                steps {
+                    sh "mvn clean install -DskipTests"
+                }
             }
         }
 
-        stage('Maven Build') {
-            steps {
-                sh "mvn clean install -DskipTests"
+        post {
+            success {
+                archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true
             }
-        }
-    }
-
-    post {
-        success {
-            archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true
         }
     }
 }
